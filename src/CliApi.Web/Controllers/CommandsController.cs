bdbcd5661ac4b1f0bb6538abc374b972d674056e
Chad Kruszynsky <ckruszynsky@gmail.com>
2020-10-14 10:15:29 -0500

    Add command repository to controller and implement methods

--- src/CliApi.Web/Controllers/CommandsController.cs
+++ src/CliApi.Web/Controllers/CommandsController.cs
@@ -1,5 +1,9 @@
+using System.Windows.Input;
+using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using CliApi.Web.Data;
+using CliApi.Web.Models;
 
 namespace CliApi.Web.Controllers
 {
@@ -7,7 +11,24 @@ namespace CliApi.Web.Controllers
     [ApiController]
     public class CommandsController : ControllerBase
     {
+        private readonly ICommandRepository _repository;
+        public CommandsController(ICommandRepository repository)
+        {
+            _repository = repository;
+        }
+
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get() => new string[] { "this", "is", "hard", "coded" };
+        public ActionResult<IEnumerable<Command>> GetAll() => Ok(_repository.GetAll());
+
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetById(int id)
+        {
+            var commandItem = _repository.GetById(id);
+            if (commandItem == null)
+            {
+                return NotFound();
+            }
+            return Ok(commandItem);
+        }
     }
 }
