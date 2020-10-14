b452d1fd437c7e56c974971a2a2e6f58e735f715
Chad Kruszynsky <ckruszynsky@gmail.com>
2020-10-14 08:56:59 -0500

    add required services for mvc support

--- src/CliApi.Web/Startup.cs
+++ src/CliApi.Web/Startup.cs
@@ -16,6 +16,7 @@ namespace CliApi.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,13 +29,8 @@ namespace CliApi.Web
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+           
         }
     }
 }
