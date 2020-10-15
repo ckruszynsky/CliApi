73a1b8ce80ed79768b4bddc4ffb3e79d7e13b018
Chad Kruszynsky <ckruszynsky@gmail.com>
2020-10-15 13:46:34 -0500

    Add SqlCommand Repository

---
+++ src/CliApi.Web/Data/SqlCommandRepository.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using CliApi.Web.Models;
+
+namespace CliApi.Web.Data
+{
+    public class SqlCommandRepository : ICommandRepository
+    {
+        public void Create(Command command)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(Command command)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Command GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Command command)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
