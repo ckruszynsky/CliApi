11134d8ef24d61aa5735435ce894dba6177b99aa
Chad Kruszynsky <ckruszynsky@gmail.com>
2020-10-16 16:46:02 -0500

    Add GetAll Controller Tests

--- test/CliApi.Web.Tests/CommandControllerTests.cs
+++ test/CliApi.Web.Tests/CommandControllerTests.cs
@@ -4,27 +4,133 @@ using Xunit;
 using System.Collections.Generic;
 using Moq;
 using AutoMapper;
-using CliApi.Web.Models;
 using CliApi.Web.Data;
-using Microsoft.EntityFrameworkCore;
 using CliApi.Web.Controllers;
+using CliApi.Web.Profiles;
+using Microsoft.AspNetCore.Mvc;
+using CliApi.Web.Dtos;
 
 namespace CliApi.Web.Tests
 {
-    public class CommandControllerTests
+    public class CommandControllerTests : IDisposable
     {
-        DbContextOptionsBuilder<CommandContext> optionsBuilder;
-        CommandContext dbContext;
-        CommandsController controller;
+
+        Mock<ICommandRepository> mockRepo;
+        CommandsProfile realProfile;
+        MapperConfiguration configuration;
+        IMapper mapper;
 
         public CommandControllerTests()
         {
+            mockRepo = new Mock<ICommandRepository>();
+            realProfile = new CommandsProfile();
+            configuration = new MapperConfiguration(cfg => cfg.AddProfile(realProfile));
+            mapper = new Mapper(configuration);
+        }
+
+        public void Dispose()
+        {
+            mockRepo = null;
+            mapper = null;
+            configuration = null;
+            realProfile = null;
+        }
+
+        //**************************************************
+        //*
+        //GET   /api/commands Unit Tests
+        //*
+        //**************************************************
+        [Fact]
+        public void GetAll_ReturnsZeroResources_WhenDBIsEmpty()
+        {
+            //Arrange 
+            mockRepo.Setup(repo => repo.GetAll()).Returns(GetCommands(0));
+
+            var controller = new CommandsController(mockRepo.Object, mapper);
+
+            //Act
+            var result = controller.GetAll();
+
+            //Assert
+            Assert.IsType<OkObjectResult>(result.Result);
 
         }
-        [Fact]
-        public void GetCommands_ReturnsZeroItems_WhenDBIsEmpty()
-        {
 
+        [Fact]
+        public void GetAll_ReturnsOneResource_WhenDBHasOneResource()
+        {
+            //Arrange 
+            mockRepo.Setup(repo =>
+              repo.GetAll()).Returns(GetCommands(1));
+
+            var controller = new CommandsController(mockRepo.Object, mapper);
+
+            //Act
+            var result = controller.GetAll();
+
+            //Assert
+            var okResult = result.Result as OkObjectResult;
+
+            var commands = okResult.Value as List<CommandReadDto>;
+
+            Assert.Single(commands);
+        }
+
+        [Fact]
+        public void GetAll_Returns200OK_WhenDBHasOneResource()
+        {
+            //Arrange 
+            mockRepo.Setup(repo =>
+              repo.GetAll()).Returns(GetCommands(1));
+
+            var controller = new CommandsController(mockRepo.Object, mapper);
+
+            //Act
+            var result = controller.GetAll();
+
+            //Assert
+            Assert.IsType<OkObjectResult>(result.Result);
+
+        }
+
+        [Fact]
+        public void GetAll_ReturnsCorrectType_WhenDBHasOneResource()
+        {
+            //Arrange 
+            mockRepo.Setup(repo =>
+              repo.GetAll()).Returns(GetCommands(1));
+
+            var controller = new CommandsController(mockRepo.Object, mapper);
+
+            //Act
+            var result = controller.GetAll();
+
+            //Assert
+            Assert.IsType<ActionResult<IEnumerable<CommandReadDto>>>(result);
+        }
+
+
+
+        //**************************************************
+        //*
+        //Private Support Methods
+        //*
+        //**************************************************
+        private List<Command> GetCommands(int num)
+        {
+            var commands = new List<Command>();
+            if (num > 0)
+            {
+                commands.Add(new Command
+                {
+                    Id = 0,
+                    HowTo = "How to genrate a migration",
+                    CommandLine = "dotnet ef migrations add <Name of Migration>",
+                    Platform = ".Net Core EF"
+                });
+            }
+            return commands;
         }
     }
 }
