f490d04de0b3e42fc036e34be0f2e2413c5de26f
Chad Kruszynsky <ckruszynsky@gmail.com>
2020-10-19 10:14:22 -0500

    add secure client to project

---
+++ src/CliApi.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CliApi.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
